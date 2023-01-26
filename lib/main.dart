75f7061680b908850c2fc7fed990e555693ccd40
Umais Qureshi <umaisqureshi26@icloud.com>
2023-01-26 15:59:55 +0100

    Final Commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:fluttertask/Theme/theme.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 import 'Route/router.dart';
 import 'firebase_options.dart';
